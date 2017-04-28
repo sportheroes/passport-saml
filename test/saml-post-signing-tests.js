c4b17f0bca1620b7fc283e16b3c2d993dcb77f27
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2017-04-28 15:48:20 +0100

    HTTP-POST signatures - Add enveloped-signature transform (required in SAML)

--- test/saml-post-signing-tests.js
+++ test/saml-post-signing-tests.js
@@ -20,6 +20,7 @@ describe('SAML POST Signing', function () {
     var result = signMessage(xml, '/SAMLRequest', signingKey, options)
     result.should.match(/<SignatureMethod Algorithm="http:\/\/www.w3.org\/2001\/04\/xmldsig-more#rsa-sha256"/)
     result.should.match(/<Transform Algorithm="http:\/\/www.w3.org\/2001\/10\/xml-exc-c14n#"\/>/)
+    result.should.match(/<Transform Algorithm="http:\/\/www.w3.org\/2000\/09\/xmldsig#enveloped-signature"\/>/)
     result.should.match(/<DigestMethod Algorithm="http:\/\/www.w3.org\/2001\/04\/xmlenc#sha256"\/>/)
   })
 })
